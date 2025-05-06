280bb858329d91afa81c7ad5eafd62271f58e054
ChristiPWright <ChristiPWright@gmail.com>
2025-05-06 13:20:48 -0700

    Init scaffold

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, Response
+from twilio.twiml.voice_response import VoiceResponse
+
+app = Flask(__name__)
+
+# start call
+
+# speech input
+
+# stream
+
+# validate
+
+# end call
+
+if __name__ == "__main__":
+    app.run(debug=True)
