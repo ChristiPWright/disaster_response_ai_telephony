b5302fde0d530fe77229055993528382761626cf
ChristiPWright <ChristiPWright@gmail.com>
2025-05-06 14:35:04 -0700

    added call Session state

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 import os
 
-from flask import Flask, request, Response
+from flask import Flask, request, session, Response
+from flask_session import Session
 from twilio.twiml.voice_response import VoiceResponse
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-secret")
+app.config["SESSION_TYPE"] = "filesystem"  # In prod: use 'redis'
+Session(app)
+
+#init state object -> TODO abstract out
+call_state = {
+        "step": 0,
+        "name": "",
+        "dob": "",
+        "insurance": "",
+        "payer_id": "",
+        "referral": "",
+        "physician": "",
+        "complaint": "",
+        "address": "",
+        "phone": "",
+        "email": "",
+        "appointment": ""
+    }
 
 # start call
 @app.route("/start_call", methods=["POST"])
 def start_call():
     # Initial Twilio webhook
-    print("Incoming call...")
+    call_sid = request.form.get("CallSid")
+    print(f"Incoming call: {call_sid}")
+    session[call_sid] = call_state
 
     response = VoiceResponse()
 
@@ -22,8 +44,10 @@ def start_call():
 @app.route("/gather", methods=['POST'])
 def gather():
     #Process speech input
+    call_sid = request.form.get("CallSid")
     transcript = request.form.get("SpeechResult", "")
     print(f"User said: {transcript}")
+    gather_call_state = session.get(call_sid, {})
 
     #TODO: AI agent logic
 
